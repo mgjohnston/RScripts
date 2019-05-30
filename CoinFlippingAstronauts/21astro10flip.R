030d9a2c2cc8b3887afa075c8d0add8ef55974cb
Matthew Johnston <49154660+mgjohnston@users.noreply.github.com>
2019-05-30 18:47:35 +0100

    Update 21astro10flip.R

--- CoinFlippingAstronauts/21astro10flip.R
+++ CoinFlippingAstronauts/21astro10flip.R
@@ -11,50 +11,7 @@ splitter <- function(values, N){
   re = rep(1:length(dif), times = dif)
   return(split(values, re))
 }
-colMax<-function(X) apply(X, 2, max)
-prive = function(v, N){ #added dummy N argument because of the tester function
-  dummy = N
-  computeD <- function(p, q, S) {
-    n <- length(S)
-    S.star <- S[n] / 3
-    if (all(p < q)) {
-      (S[p] - S.star)^2 + (S[q] - S[p] - S.star)^2 + (S[n] - S[q] - S.star)^2
-    } else {
-      stop("You shouldn't be here!")
-    }
-  }
-  optiCut <- function(v, N) {
-    S <- cumsum(v)
-    n <- length(v)
-    S_star <- S[n] / 3
-    # good starting values
-    p_star <- which.min((S - S_star)^2)
-    q_star <- which.min((S - 2*S_star)^2)
-    print(min <- computeD(p_star, q_star, S))
-    
-    count <- 0
-    for (q in 2:(n-1)) {
-      S3 <- S[n] - S[q] - S_star
-      if (S3*S3 < min) {
-        count <- count + 1
-        D <- computeD(seq_len(q - 1), q, S)
-        ind = which.min(D);
-        if (D[ind] < min) {
-          # Update optimal values
-          p_star = ind;
-          q_star = q;
-          min = D[ind];
-        }
-      }
-    }
-    c(p_star, q_star, computeD(p_star, q_star, S), count)
-  }
-  z3 = optiCut(v)
-  inds = c(0, z3[1:2], length(v))
-  dif = diff(inds)
-  re = rep(1:length(dif), times = dif)
-  return(split(v, re))
-} #added output to be more in line with the other two
+
 people<-21
 mintosses <- ceiling(log2(people))
 maxtosses<-10
